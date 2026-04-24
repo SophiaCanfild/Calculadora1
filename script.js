179bad66e7f32509b35690618602bb24de34e833
sophia <sophia.saladini@escola.pr.gov.br>
2026-04-24 14:34:06 -0300

    1

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const visor = document.getElementById('resultado');
+
+function inserir(valor) {
+    visor.value += valor;
+}
+
+function limpar() {
+    visor.value = "";
+}
+
+function calcular() {
+    try {
+        // A função eval() resolve a expressão matemática dentro do visor
+        visor.value = eval(visor.value);
+    } catch (e) {
+        visor.value = "Erro";
+        setTimeout(limpar, 1500); // Limpa o erro após 1.5s
+    }
+}
 
